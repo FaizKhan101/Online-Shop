6b2483004664f396e2c00950df23e63ea3d30caa
Faiz <faizkhan8225@gmail.com>
2023-09-26 18:44:29 +0530

    Add login for add cart item

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const addCsrfTokenMiddleware = require("./middlewares/csrf-token")
 const errorHandlerMiddleware = require("./middlewares/error-handler")
 const checkAuthStatusMiddleware = require("./middlewares/check-auth")
 const protectRoutesMiddleware = require("./middlewares/protect-routes")
+const cartMiddleware = require("./middlewares/cart")
 
 const app = express();
 
@@ -29,6 +30,7 @@ const sessionConfig = createSessionConfig()
 app.use(expressSession(sessionConfig))
 
 app.use(csrf())
+app.use(cartMiddleware)
 
 app.use(addCsrfTokenMiddleware)
 app.use(checkAuthStatusMiddleware)
---
+++ controllers/cart.controller.js
@@ -0,0 +1,17 @@
+const Product = require("../model/product.model")
+
+exports.addCartItem = async (req, res, next) => {
+    let product;
+    try {
+        product = await Product.findById(req.body.productId)
+    } catch (error) {
+        return next(error)
+    }
+    const cart = res.locals.cart;
+    cart.addItem(product)
+    req.session.cart = cart;
+    res.status(201).json({
+        message: "Cart updated!",
+        newTotalItems: cart.totalQuantity
+    })
+}
